80c532b306f12105adb9d577e8ffc9f46d5e4a15
spinarak1 <spinarak57@gmail.com>
2019-04-06 18:25:56 +0200

    Create project

---
+++ src/main/java/com/software/rateit/RateitApplication.java
@@ -0,0 +1,13 @@
+package com.software.rateit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateitApplication.class, args);
+    }
+
+}
