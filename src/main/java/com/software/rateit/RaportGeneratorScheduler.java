99f96f4fb00470a18fb243a6e84e11a3af6e23ef
spinarak1 <spinarak57@gmail.com>
2019-04-16 02:10:41 +0200

    Add RaportGenerator(Bug)

---
+++ src/main/java/com/software/rateit/RaportGeneratorScheduler.java
@@ -0,0 +1,34 @@
+package com.software.rateit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class RaportGeneratorScheduler {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private static final Logger log = LoggerFactory.getLogger(RaportGeneratorScheduler.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    
+    @Scheduled(cron = "0 * * * * *")
+    public void reportCurrentTime() {
+        System.out.println("XDD");
+        log.info("The time is now {}", dateFormat.format(new Date()));
+        try {
+            RaportGenerator generator = new RaportGenerator();
+            generator.sumUsersRegistrationOfYesterday(entityManager, "C:\\Users\\spina\\Desktop\\XML\\");
+        }catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+        } finally {
+        }
+    }}
