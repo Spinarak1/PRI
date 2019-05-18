2609d972cc44e7f0df5dafae05a8d486ab57af0f
Spinarak <spinarak57@gmail.com>
2019-05-18 03:39:54 +0200

    Fix filter add exception

--- src/main/java/com/software/rateit/repositories/ArtistRepository.java
+++ src/main/java/com/software/rateit/repositories/ArtistRepository.java
@@ -1,8 +1,10 @@
 package com.software.rateit.repositories;
 
 import com.software.rateit.Entity.Artist;
+import com.software.rateit.Entity.CD;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ArtistRepository extends PagingAndSortingRepository<Artist,Long> {
     Artist findByStageName(String stageName);
+    Artist findByCd(CD cd);
 }
