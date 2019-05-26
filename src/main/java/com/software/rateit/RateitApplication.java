4156a9e62de3ba7095821ae4b2664d168555ee97
Spinarak <spinarak57@gmail.com>
2019-05-26 21:45:59 +0200

    fix

--- src/main/java/com/software/rateit/RateitApplication.java
+++ src/main/java/com/software/rateit/RateitApplication.java
@@ -3,33 +3,11 @@ package com.software.rateit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Collections;
 
 @SpringBootApplication
 public class RateitApplication extends SpringBootServletInitializer {
-    @Bean
-    public CommandLineRunner setupDefaultUser(UserService service) {
-        return args -> {
-            service.save(new User(
-                    "user", //username
-                    "user", //password
-                    Arrays.asList(new Role("USER"), new Role("ACTUATOR")),//roles
-                    true//Active
-            ));
-        };
-    }
-    @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(RateitApplication.class);
@@ -37,30 +15,5 @@ public class RateitApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RateitApplication.class, args);
-    }
-    @Bean
-    public FilterRegistrationBean simpleCorsFilter() {
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowCredentials(true);
-
-        // *** URL below needs to match the Vue client URL and port ***
-
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:8081"));
-
-        config.setAllowedMethods(Collections.singletonList("*"));
-
-        config.setAllowedHeaders(Collections.singletonList("*"));
-
-        source.registerCorsConfiguration("/**", config);
-
-        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
-
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
-        return bean;
-
     }
 }
