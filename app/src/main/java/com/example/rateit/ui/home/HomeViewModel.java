75051a1ecbff91fe83db5b8d851bbb049a9b28f4
Spinarak <spinarak57@gmail.com>
2019-10-03 21:46:01 +0200

    String init

--- app/src/main/java/com/example/rateit/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/rateit/ui/home/HomeViewModel.java
@@ -4,13 +4,16 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("HOME");
     }
 
     public LiveData<String> getText() {
